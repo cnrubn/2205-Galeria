1c9f4ff77afdd65d8dcc1d29628d6803ee770862
Rubén <cnrubn@gmail.com>
2022-05-13 16:21:11 +0200

    Modo oscuro permanente con localstorage.

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ const btnModos = document.querySelector('#modos');
 btnModos.addEventListener('click', () => {
     document.body.classList.toggle('oscuro');
     btnModos.classList.toggle('activo');
+
+    // GUARDADO EN LOCALSTORAGE
+    if(document.body.classList.contains('oscuro')){
+        localStorage.setItem('tema-oscuro','true');
+    } else {
+        localStorage.setItem('tema-oscuro','false');
+    }
 });
+
+// COMPROBAR Y OBTENER EL MODO ACTUAL
+if(localStorage.getItem('tema-oscuro') === 'true') {
+    document.body.classList.add('oscuro');
+    btnModos.classList.add('activo');
+} else {
+    document.body.classList.remove('oscuro');
+    btnModos.classList.remove('activo');
+}
